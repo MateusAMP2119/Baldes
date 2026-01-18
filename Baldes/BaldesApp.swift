989eb344483df2857ce87dca4cf0b8f2893e7de1
Mateus Costa <105391128+MateusAMP2119@users.noreply.github.com>
2026-01-18 18:26:41 +0000

    Initial Commit

---
+++ Baldes/BaldesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaldesApp.swift
+//  Baldes
+//
+//  Created by Mateus Costa on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BaldesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaldesTests/BaldesTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaldesTests.swift
+//  BaldesTests
+//
+//  Created by Mateus Costa on 18/01/2026.
+//
+
+import Testing
+@testable import Baldes
+
+struct BaldesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
