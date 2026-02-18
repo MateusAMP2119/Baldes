21cb23f289288704a2c1eaf3e7c129c8fa2adff4
Mateus Costa <105391128+MateusAMP2119@users.noreply.github.com>
2026-02-18 11:37:14 +0000

    Initial Commit

---
+++ Baldes/BaldesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaldesApp.swift
+//  Baldes
+//
+//  Created by Mateus Costa on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BaldesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
